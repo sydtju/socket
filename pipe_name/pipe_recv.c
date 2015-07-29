c01fad8c5e242841cbc6d110058df4b9bbfd50eb
yadongshao <348829719@qq.com>
2015-07-29 11:56:02 +0800

    Create pipe_recv.c

---
+++ pipe_name/pipe_recv.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<fcntl.h>
+#define BUF_SIZE  80
+int main(int argc,char *argv[]){
+	int infile;
+	int count=1;
+	char buf[BUF_SIZE];
+	if(mkfifo("myfifo",0777)<0) //后一个参数是name-pipe的权限
+	{
+		if(errno==EEXIST)//管道已经存在
+		{
+			fprintf(stdout,"the fifo exist.\n");
+		}
+		else{
+			fprintf(stdout,"create myfifo is failure!\n");
+			exit(-1);
+		}
+	}
+	else{
+		fprintf(stdout,"create fifo is success! \n");
+	}
+	infile=open("myfifo",O_RDONLY);
+	if(infile<0) {fprintf(stdout,"open is error!\n");exit(-1);}
+	while(count=read(infile,buf,BUF_SIZE)>0)
+	{
+		fprintf(stdout,"recvfrom fifo:%s\n",buf);
+		memset(buf,0,BUF_SIZE);
+	}
+	close(infile);
+	return 0;
+
+}
